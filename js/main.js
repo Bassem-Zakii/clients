7db65240053376209596c6d2f95e06a6b50d587e
Bassem-Zakii <36578795+Bassem-Zakii@users.noreply.github.com>
2021-10-14 00:25:59 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,7 +7,28 @@ const menuList = document.querySelector(".logo-menu nav ul");
 
 navBtn.addEventListener("click", () => {
   menuList.classList.toggle("active");
+  navBtn.classList.toggle("active");
 });
+
+// active link in menu
+
+const navLinks = document.querySelectorAll(".logo-menu nav li a");
+
+navLinks.forEach((links) => {
+  links.addEventListener("click", function () {
+    navLinks.forEach((link) => link.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+// navLinks.forEach((links) => {
+//   links.addEventListener("click", function () {
+//     links.forEach((link) => {
+//       link.classList.remove("active");
+//     });
+//     this.classList.add("active");
+//   });
+// });
 
 // section question-answer show and hide each question
 
