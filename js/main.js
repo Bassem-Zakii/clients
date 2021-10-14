a8099cde8ba9c37f35873b11e024c0211ccd4117
Bassem-Zakii <36578795+Bassem-Zakii@users.noreply.github.com>
2021-10-14 23:00:27 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,17 +10,38 @@ navBtn.addEventListener("click", () => {
   navBtn.classList.toggle("active");
 });
 
+// when you click outside navbar and navbar open button remove class active
+
+const html = document.querySelector("html");
+
+html.addEventListener("click", function (e) {
+  if (e.target !== navBtn && e.target !== menuList) {
+    navBtn.classList.remove("active");
+    menuList.classList.remove("active");
+  }
+});
+
 // active link in menu
 
 const navLinks = document.querySelectorAll(".logo-menu nav li a");
+const section = document.querySelectorAll("section");
 
-navLinks.forEach((links) => {
-  links.addEventListener("click", function () {
-    navLinks.forEach((link) => link.classList.remove("active"));
-    this.classList.add("active");
+window.addEventListener("scroll", () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 255;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        let anchor = document.querySelector(`header nav a[href*="${id}"]`);
+        anchor.classList.add("active");
+      });
+    }
   });
 });
-
 
 // section question-answer show and hide each question
 
