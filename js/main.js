7c191a068c15a44f05648e8071b916bf964ea598
Bassem-Zakii <36578795+Bassem-Zakii@users.noreply.github.com>
2021-11-04 19:23:01 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 "use strict";
 
 const openOnlineList = document.querySelector(".open-online-user");
-const onlineList = document.querySelector(".online-chat ");
+const onlineList = document.querySelector(".online-chat");
 
 openOnlineList.addEventListener("click", () => {
   openOnlineList.classList.toggle("active");
   onlineList.classList.toggle("active");
+});
+
+// CLoseing online list by click outside
+
+const html = document.querySelector("html");
+
+html.addEventListener("click", function (e) {
+  const removeOnlineList = e.target.closest(".open-online-user");
+  if (!removeOnlineList && e.target !== onlineList) {
+    openOnlineList.classList.remove("active");
+    onlineList.classList.remove("active");
+  }
 });
